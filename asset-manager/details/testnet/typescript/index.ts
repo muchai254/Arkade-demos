90f85b53c6efcdfa4c083e263efaf98434e7e839
gringokiwi <9779954+gringokiwi@users.noreply.github.com>
2026-06-27 16:53:19 -0300

    feat: use dummy wallet identity in asset-manager/details for simplicity

--- asset-manager/details/testnet/typescript/index.ts
+++ asset-manager/details/testnet/typescript/index.ts
@@ -1,28 +1,20 @@
 import {
   InMemoryContractRepository,
   InMemoryWalletRepository,
-  ReadonlyDescriptorIdentity,
   ReadonlyWallet,
   RestArkProvider,
-  RestDelegateProvider,
+  SingleKey,
 } from "@arkade-os/sdk";
 import { hex, utf8 } from "@scure/base";
 
 const ASSET_ID =
   "9fcd56ae25d2278fd2be1c37d99f8c0420624634f3ba5300a703408b700948660000" as const;
-const WALLET_DESCRIPTOR =
-  `tr([73c5da0a/86'/1'/0']tpubDDfvzhdVV4unsoKt5aE6dcsNsfeWbTgmLZPi8LQDYU2xixrYemMfWJ3BaVneH3u7DBQePdTwhpybaKRU95pi6PMUtLPBJLVQRpzEnjfjZzX/0/*)` as const;
 const OPERATOR_URL = "https://mutinynet.arkade.sh" as const;
-const DELEGATE_URL = "https://delegator.mutinynet.arkade.sh" as const;
 
-/** 1. Create read-only identity */
-const identity = ReadonlyDescriptorIdentity.fromDescriptor(WALLET_DESCRIPTOR);
-
-/** 2. Create read-only wallet */
+/** 1. Create read-only wallet with dummy identity */
 const wallet = await ReadonlyWallet.create({
-  identity,
+  identity: await SingleKey.fromRandomBytes().toReadonly(),
   arkProvider: new RestArkProvider(OPERATOR_URL),
-  delegateProvider: new RestDelegateProvider(DELEGATE_URL),
   /**
    * Explicitly use in-memory storage
    * Defaults to IndexedDB if undefined
@@ -33,7 +25,7 @@ const wallet = await ReadonlyWallet.create({
   },
 });
 
-/** 3. Fetch asset details */
+/** 2. Fetch asset details */
 const assetDetails = await wallet.assetManager.getAssetDetails(ASSET_ID);
 
 if (!assetDetails.controlAssetId) {
@@ -48,7 +40,7 @@ if (!(assetDetails.metadata && "customField" in assetDetails.metadata)) {
   });
 }
 
-/** 4. Fetch control asset details */
+/** 3. Fetch control asset details */
 const controlAssetDetails = await wallet.assetManager.getAssetDetails(
   assetDetails.controlAssetId,
 );
@@ -67,7 +59,7 @@ if (
   );
 }
 
-/** 5. Summarize asset details */
+/** 4. Summarize asset details */
 console.log({
   assetId: assetDetails.assetId,
   supply: assetDetails.supply,
