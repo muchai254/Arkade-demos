49937e5c4ba8f56e68f2fe0c786cd1d01fb25f77
gringokiwi <9779954+gringokiwi@users.noreply.github.com>
2026-06-28 01:10:09 -0300

    chore: spacing

--- indexer/outputs/by-outpoint/testnet/typescript/index.ts
+++ indexer/outputs/by-outpoint/testnet/typescript/index.ts
@@ -4,6 +4,7 @@ const OUTPOINT = {
   txid: "6445acfc889873dedf9512c63518010a89d004882ba681d905519fabf21889f0",
   vout: 0,
 } as const;
+
 const OPERATOR_URL = "https://mutinynet.arkade.sh" as const;
 
 /** 1. Connect to indexer */
